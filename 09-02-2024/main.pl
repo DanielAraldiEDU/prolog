24699bd44067a8a106f30ba2e1f7b7fb5ccae661
Daniel Sansão Araldi <100477637+DanielAraldiEDU@users.noreply.github.com>
2024-02-09 21:31:21 -0300

    feat: add data to make queries about progenitress

--- 09-02-2024/main.pl
+++ 09-02-2024/main.pl
@@ -15,8 +15,7 @@ animal(rex).
 animal(bolota).
 
 % Create rules
-% "X" is animal if it this same "X"
-% is a dog
+% "X" is animal if it is a dog too
 animal(X) :- dog(X).
 
 % Tomba and Rex are dogs
@@ -34,3 +33,25 @@ cat(bolota).
 % an animal and a dog
 % animal(bolota) -> false because
 % "bolota" isn't a dog.
+%-----------------------------------
+
+% "maria" is a mom of "jose"
+progenitress(maria, jose).
+% "joao" is dad of "jose"
+progenitress(joao, jose).
+% "joao" is dad of "ana"
+progenitress(joao, ana).
+% "jose" is dad of "julia"
+progenitress(jose, julia).
+% "jose" is dad of "iris"
+progenitress(jose, iris).
+% "iris" is mom of "jorge"
+progenitress(iris, jorge).
+
+% Queries:
+% progenitress(ana, jorge) -> false
+% because ana ins't mom of "jorge"
+% progenitress(Parents, iris) -> Show
+% parents of the "iris"
+% progenitress(Parents, jose) -> Show
+% parents of the "jose"
