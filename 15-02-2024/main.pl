799d00f546c9628f75555929482fee812baae31d
Daniel Sansão Araldi <100477637+DanielAraldiEDU@users.noreply.github.com>
2024-02-15 20:41:48 -0300

    feat: add conditionals to check if a value is ancestral and if it's successor

--- 15-02-2024/main.pl
+++ 15-02-2024/main.pl
@@ -1,5 +1,30 @@
-animal(X) :- cat(X).
-animal(X) :- dog(X).
+father(david, john).
+father(jim, david).
+father(steve, jim).
+father(nathan, steve).
 
-cat(bolota).
-dog(rex).
+% Check if it's grandfather
+grandfather(X, Y) :- father(X, Z), father(Z, Y).
+
+% Check if it's ancestral (non-recursive call)
+% ancestral(A, B) :- father(A, B).
+% ancestral(A, B) :- father(A, X), father(X, B).
+% ancestral(A, B) :- father(A, X), father(X, Y), father(Y, B).
+% ancestral(A, B) :- father(A, X), father(X, Y), father(Y, Z), father(Z,B).
+
+% Check if it's ancestral (recursive call)
+ancestral(X, Y) :- father(X, Y).
+ancestral(X, Y) :- father(X, Z), ancestral(Z, Y).
+% ----------------------------------------------
+
+successor(2, 1).
+successor(3, 2).
+successor(4, 3).
+successor(5, 4).
+successor(6, 5).
+successor(7, 6).
+
+% Check if it's successor
+bigger(X, Y) :- successor(X, Y).
+bigger(X, Y) :- successor(X, Z), bigger(Z, Y).
+% ----------------------------------------------
