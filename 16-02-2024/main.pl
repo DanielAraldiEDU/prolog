cb6181881fd88a11444064857d3bbe4d4b06253e
Daniel Sansão Araldi <100477637+DanielAraldiEDU@users.noreply.github.com>
2024-02-16 19:52:20 -0300

    feat: create function to check if a term is pair and add auxiliary comments about operators

---
+++ 16-02-2024/main.pl
@@ -0,0 +1,9 @@
+% Check if a number is pair
+is_pair(X) :- X mod 2 =:= 0.
+% The "=:=" operator check if two values
+% are equals
+% The "=" operator check if two values
+% are identicals!!!
+% The "is" operator check if term on the
+% right is equal the left term
+% -----------------------------------------
