b52285ec031eea2e0cd2fd02d187ec6ca1f3937d
Daniel Sansão Araldi <100477637+DanielAraldiEDU@users.noreply.github.com>
2024-02-16 21:08:47 -0300

    feat: create `factorial` and `is_palindrome` functions

--- 16-02-2024/main.pl
+++ 16-02-2024/main.pl
@@ -3,7 +3,25 @@ is_pair(X) :- X mod 2 =:= 0.
 % The "=:=" operator check if two values
 % are equals
 % The "=" operator check if two values
-% are identicals!!!
-% The "is" operator check if term on the
-% right is equal the left term
+% are identical!!!
+% The "is" operator checks if term on the
+% right is equal to the left term
 % -----------------------------------------
+
+factorial(0, 1).
+factorial(X, Result) :-
+	X > 0,
+	Y is X - 1,
+	factorial(Y, SubResult),
+	Result is X * SubResult.
+% -----------------------------------------
+
+reverse_list([], []).
+reverse_list([H|T], Reverse) :-
+  reverse_list(T, ReverseTail),
+  append(ReverseTail, [H], Reverse).
+% The "append" function makes concatenation
+% between arrays
+
+is_palindrome(List) :-
+  reverse_list(List, List).
